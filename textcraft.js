24d951fca5437db86188bf695edabcde2d4247b3
U-AETV\TDemuyt <TDemuyt@NY-L509-514998.aetvn.com>
2014-08-13 16:45:22 -0400

    IIFE 4TW

--- textcraft.js
+++ textcraft.js
@@ -1,1 +1,37 @@
-console.log('All is well');
+
+//The Approach; everything starts here, once the code size becomes unacceptable, we modularize into
+//separate js files, require.js is overkill, I will just have a long list of js files in the main
+//html files
+
+/*Template for an IFFE
+var myModule = (function () {
+ 
+  //Private 
+   
+  //Public 
+  return {
+      
+  };
+ 
+})();
+*/
+
+
+
+
+var player = (function () {
+ 
+  //Private
+  var inventory = [];
+  
+   
+  //Public 
+  return {
+    
+      
+  };
+ 
+})();
+
+
+console.log( player , 'All is well');
